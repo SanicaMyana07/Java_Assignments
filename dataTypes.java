a5b18bd691a6e2c170c39096646587c7edfe8b94
SanicaMyana07 <myanasanica@gmail.com>
2024-07-19 14:57:46 +0530

    Add files via upload

---
+++ dataTypes.java
@@ -0,0 +1,12 @@
+public class dataTypes {
+    public static void main(String args[]) {
+        System.out.println("Sizes of data types");
+        System.out.println("Byte : " + Byte.SIZE); // we can find size of datatypes by using .SIZE.
+        System.out.println("short : " + Short.SIZE);
+        System.out.println("double : " + Double.SIZE);
+        System.out.println("long : " + Long.BYTES); // we can find size of datatypes by using .BYTES also
+        System.out.println("Float : " + Float.BYTES);
+        System.out.println("character : " + Character.SIZE);
+        System.out.println("Integer : " + Integer.SIZE);
+    }
+}
